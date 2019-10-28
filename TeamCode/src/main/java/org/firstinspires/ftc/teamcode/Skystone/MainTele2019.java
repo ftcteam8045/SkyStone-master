6f53515801dce2ccb28722ac5b3652377c22d9d6
ftcteam8045 <pleasanthillftc@gmail.com>
2019-10-27 18:13:07 -0700

    ok

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/MainTele2019.java
@@ -0,0 +1,197 @@
+package org.firstinspires.ftc.teamcode.Skystone;
+
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+@TeleOp(name = "MainTele", group = "8045")  // @Autonomous(...) is the other common choice
+@Disabled
+public class MainTele2019 extends OpMode {
+
+    Hardware2019 Cosmo;
+    private ElapsedTime runtime = new ElapsedTime();
+    double timeLeft;
+    // variables used during the configuration process
+
+
+
+//    double turnDirection;
+    public double topSpeed = 0.5 ;
+
+
+    //Back mode
+    public boolean frontIsForward = false;
+    public boolean rightbtnIsReleased = true;
+
+    //Drive type
+
+    public boolean run = false;
+
+//    Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorDistance;
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    public float hsvValues[] = {0F, 0F, 0F};
+    // values is a reference to the hsvValues array.
+    public float values[] = hsvValues;
+
+
+
+
+
+
+    public double multiplier = 0.1818;
+
+
+
+    @Override
+    public void init() {
+        Cosmo = new Hardware2019();
+        Cosmo.init(hardwareMap);
+
+
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+
+
+
+        telemetry.addData("Finished", "Initializing");
+        telemetry.update();
+
+
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+
+        timeLeft = 120 - runtime.seconds();
+
+//        // BACK mode
+
+        if (gamepad1.right_stick_button) {
+            if (rightbtnIsReleased) {
+                rightbtnIsReleased = false;
+                frontIsForward = !frontIsForward;
+            }
+        } else {
+            rightbtnIsReleased = true;
+        }
+
+
+        /**  set lights color  **/
+        /** 2M Distance Sensor Code  */
+
+//        Color.RGBToHSV((int)(Cosmo.sensorColor.red() * 255), (int)(Cosmo.sensorColor.green() * 255), (int)(Cosmo.sensorColor.blue() * 255), hsvValues);
+//        if (hsvValues[0] > grayRedBorder && hsvValues[0] < grayBlueBorder ) {
+//            Cosmo.LEDDriver.setPattern(teamColor);
+//        } else {
+//            Cosmo.LEDDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.WHITE);
+//        }
+
+
+        /**  set drive speed  **/
+
+        if (gamepad1.left_bumper) {
+            topSpeed = 1.0;
+        } else if (gamepad1.left_trigger > 0.1) {
+            topSpeed = 0.3;
+        }
+        else {
+            topSpeed = 0.6;
+        }
+//        /** set driving colors **/
+//        if (topSpeed == 0.4) {
+//            Cosmo.LEDDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.DARK_BLUE);
+//        } else {
+//            Cosmo.LEDDriver.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+//        }
+        /** DRIVE  HERE   */
+
+        drivesmart(-gamepad1.right_stick_x, -gamepad1.right_stick_y, gamepad1.left_stick_x);
+
+
+        telemetry.addData("range", String.format("%.01f mm", Cosmo.sensor.getDistance(DistanceUnit.MM)));
+
+
+        telemetry.update();
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * This method puts the current thread to sleep for the given time in msec.
+     * It handles InterruptException where it recalculates the remaining time
+     * and calls sleep again repeatedly until the specified sleep time has past.
+     *
+     * @param sleepTime specifies sleep time in msec.
+     */
+    public static void sleep(long sleepTime) {
+        long wakeupTime = System.currentTimeMillis() + sleepTime;
+
+        while (sleepTime > 0) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+            }
+            sleepTime = wakeupTime - System.currentTimeMillis();
+        }
+    }   //sleep
+
+
+    public void drivesmart(double x, double y, double turn) {
+
+        if (frontIsForward) {             // driving with the front facing forward
+
+        } else {                            // driving with the rear facing forward
+            y = -y;
+            x = -x;
+        }
+
+        double lfpower;
+        double lrpower;
+        double rfpower;
+        double rrpower;
+
+        double rotation = turn;  // knock down the turn power -- NOT ANYMORE
+
+        //Determine largest power being applied in either direction
+
+        lfpower = ( y - x + rotation);
+        lrpower = ( y + x + rotation);
+        rfpower = ( y + x - rotation);
+        rrpower = ( y - x - rotation);
+
+        lfpower = lfpower * topSpeed;
+        lrpower = lrpower * topSpeed;
+        rfpower = rfpower * topSpeed;
+        rrpower = rrpower * topSpeed;
+
+        Cosmo.leftFront.setPower(lfpower);
+        Cosmo.leftRear.setPower(lrpower);
+        Cosmo.rightFront.setPower(rfpower);
+        Cosmo.rightRear.setPower(rrpower);
+
+    }
+//
+
+
+    public void stop() {
+        Cosmo.leftFront.setPower(0.0);
+        Cosmo.leftRear.setPower(0.0);
+        Cosmo.rightFront.setPower(0.0);
+        Cosmo.rightRear.setPower(0.0);
+    }
+
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skystone/testHardware.java
@@ -0,0 +1,217 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Skystone;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ *
+ *
+ */
+public class testHardware
+{
+    /* Public OpMode members. */
+    //public DcMotor  leftFront   = null;
+    //public DcMotor  rightFront  = null;
+//    public DcMotor  leftRear    = null;
+//    public DcMotor  rightRear   = null;
+//    public Servo    fServoL  = null;
+//    public Servo    fServoR  = null;
+
+    //    public Servo    rightClaw   = null;
+
+    //public com.qualcomm.hardware.rev.RevBlinkinLedDriver.BlinkinPattern LEDpattern;
+
+    // The IMU sensor object
+    public BNO055IMU imu;
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+
+
+
+    public final double turn_THRESHOLD = 2.0;
+    public static final double drive_THRESHOLD = 1.0;
+    public final double turn_MIN_SPEED = 0.15;
+    public final double turn_COEF = 1.0;
+    public static final double drive_COEF = 1.0; //Maximum additional speed to add to a motor during a gyro drive
+
+    ////    public final double WHEEL_DIAMETER = 4.0;
+////    public final double GEAR_RATIO = 24/24;
+////    public final double TICKS_REV = 537.6;
+////    public final double COUNTS_PER_INCH = (TICKS_REV * GEAR_RATIO) / (WHEEL_DIAMETER * 3.1415);
+    public final double COUNTS_PER_INCH = 72;   // Yellowjacket 223 24/32 gear ratio   100 inches drove only 62
+//    public final double COUNTS_PER_INCH = 60;   // Yellowjacket 223 32/32 gear ratio
+//    public final double COUNTS_PER_INCH = 32;   // Neverest 20 orbitals 24/32 gear ratio
+//    public final double COUNTS_PER_INCH = 42;   // Neverest 20 orbitals 32/32 gear ratio
+//    public final double COUNTS_PER_INCH = 114;  // Tetrix Direct
+//    public final double COUNTS_PER_CM = COUNTS_PER_INCH / 2.54 ;
+
+//    public static final double MID_SERVO       =  0.5 ;
+//    public static final double ARM_UP_POWER    =  0.45 ;
+//    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    //DistanceSensor sensor;
+
+
+    /* Constructor */
+    public testHardware(){
+
+
+
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+
+
+        // LED lights
+        //LEDpattern =  com.qualcomm.hardware.rev.RevBlinkinLedDriver.BlinkinPattern.RAINBOW_RAINBOW_PALETTE;
+
+
+        // Define and Initialize Motors
+        //leftFront  = hwMap.get(DcMotor.class, "left_front");
+       // rightFront = hwMap.get(DcMotor.class, "right_front");
+//        leftRear  = hwMap.get(DcMotor.class, "left_rear");
+//        rightRear = hwMap.get(DcMotor.class, "right_rear");
+
+        // this should be for neverest  & using it for Matrix/yellowjackets as well.
+       // leftFront.setDirection(DcMotor.Direction.FORWARD);
+//        leftRear.setDirection(DcMotor.Direction.FORWARD);
+//      //  rightFront.setDirection(DcMotor.Direction.REVERSE);
+//        rightRear.setDirection(DcMotor.Direction.REVERSE);
+
+
+//        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+      //  leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+      //  rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+////this is for Tetrix
+//        leftFront.setDirection(DcMotor.Direction.REVERSE);
+//        leftRear.setDirection(DcMotor.Direction.REVERSE);
+//        rightFront.setDirection(DcMotor.Direction.FORWARD);
+//        rightRear.setDirection(DcMotor.Direction.FORWARD);
+
+
+//        liftmotor.setDirection (DcMotor.Direction.FORWARD);  //   old configuration
+
+      //  leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//      //  rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+
+
+        // Set all motors to zero power
+      //  leftFront.setPower(0);
+       // rightFront.setPower(0);
+//        leftRear.setPower(0);
+//        rightRear.setPower(0);
+
+
+
+        // Define and Initialize Motors
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+//        leftRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        rightRear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+     //   leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+     //   rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+
+
+        // Define and initialize ALL installed servos.
+//        fServoL = hwMap.get(Servo.class, "fServo_R");
+//        fServoR = hwMap.get(Servo.class, "fServo_L");
+
+//        sweepServo = hwMap.get(CRServo.class, "sweeper"  );
+        /**
+         * IMU SETUP
+         */
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. port 0 on rev hub
+        // and named "imu".
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
+
+        //sensor = hwMap.get(DistanceSensor.class, "sensor");
+
+
+
+    }
+
+
+}
+
